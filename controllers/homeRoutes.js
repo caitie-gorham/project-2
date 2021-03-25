8872d2e9c50e855e1971060b28abb291e68bdf82
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-25 19:30:41 -0400

    creating a signup file within view folder and public folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,4 +41,14 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("signup");
+});
+
 module.exports = router;
