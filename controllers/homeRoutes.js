4290af344749a144b23adbe63240c51478a1aa92
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-25 21:19:08 -0400

    setup the login, signup and welcome files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,6 +31,7 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
+
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
