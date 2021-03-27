ac25a00f86d4aec1e158a921b8bcdae3ae47e888
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-27 13:55:26 -0400

    created a framweork/template for what we're expecting to populate in our greenhouse

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,6 +31,24 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
+router.get("/houseplants", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("houseplants", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
