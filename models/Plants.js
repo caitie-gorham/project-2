c1883d3dffc2cd8efbbdccb46475d68c69adcd0c
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-30 21:06:24 -0400

    jack saved our lives or at leaast 10 hrs of it

--- models/Plants.js
+++ models/Plants.js
@@ -3,9 +3,7 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class Plants extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+    
 }
 
 Plants.init({
